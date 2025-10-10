97dbc813cdbb89f94c98a2f5a845e45d0792d2bf
Izan Rodrigues <izanrodrigues999@gmail.com>
2025-10-10 17:33:36 -0300

    update root layout: add toaster

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { Toaster } from '@skeletonlabs/skeleton-svelte';
+	import { toaster } from '@lib/svelte-toaster';
 
 	let { children } = $props();
 </script>
@@ -8,5 +10,7 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<Toaster {toaster} />
 
 {@render children?.()}
